d1dedcd2c9a997188f6e5806cfc20d0addecec2b
Florian <florian.krause001@gmail.com>
2024-06-11 23:52:10 +0200

    Added calculation of timestamps and plotting with timestamp on x-axis, also added TO-DO's for the outlier detection

--- analysing_h5.py
+++ analysing_h5.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy
 import matplotlib.pyplot as plt
 
-filename = r"C:\Users\flori\OneDrive\Documents\University of Amsterdam\Internship\DeepLabCutData\firstVideoDLC_resnet50_firstVersionMay23shuffle1_10000.h5"
+filename = r"D:\OneDrive\Documents\University of Amsterdam\Internship\DeepLabCutData\firstVideoDLC_resnet50_firstVersionMay23shuffle1_10000.h5"
 
 
 h5_df = pd.read_hdf(filename)
@@ -30,6 +30,18 @@ eye_east_y = h5_df['EyeEast']['y']
 eye_east_likelihood = h5_df['EyeEast']['likelihood']
 
 
+# Adding timestamps to frames based on frame rate of video
+frame_rate = 30
+total_frames = len(h5_df)
+timestamp_sec = []
+timestamp_min = []
+for frames in range(total_frames):
+    timestamp_sec.append(frames/frame_rate)
+    timestamp_min.append(frames/frame_rate/60)
+h5_df['Timestamp', 'Seconds'] = timestamp_sec
+h5_df['Timestamp', 'Minutes'] = timestamp_min
+
+
 # Calculating pupil diameter and writing it in a list
 diameter_df = []
 for i in range(len(h5_df.index)):
@@ -47,19 +59,18 @@ h5_df['EyeTotal', 'Diameter'] = diameter_df
 #pre_max = h5_df['EyeTotal', 'Diameter'].max()
 
 # Outlier detection based on "arbitrary" borders and creation of a cleaned DF by excluding outliers
+# TO DO:
+#   - instead of dropping the data, first determine start and end of actual trials, then interpolate dropped values
+#   - add "blink" marker, which identifies when the eyelids are closed to also interpolate these data points
+
 outlier_condition = (h5_df['EyeTotal', 'Diameter'] < 22) | (h5_df['EyeTotal', 'Diameter'] > 48)
 outlier_values = h5_df.loc[outlier_condition, ('EyeTotal', 'Diameter')]
-clean_df = h5_df.drop(index= outlier_values.index)
+clean_df = h5_df.drop(index=outlier_values.index)
 print(clean_df.head(n=10).to_string(index=False))
-
+# print(clean_df[clean_df['EyeNorth']['likelihood'] < 0.96])
 
 # Plotting the Diameter over time
-graph = clean_df.loc[:, ('EyeTotal', 'Diameter')].plot.line()
-graph.plot()
-plt.show()
-
-#graph = h5_df.loc[:, ('EyeTotal', 'Diameter')].plot.line()
-graph.plot()
+graph = clean_df.plot(x=('Timestamp', 'Minutes'), y=('EyeTotal', 'Diameter'), kind='line')
 plt.show()
 
 
