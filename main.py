ee04a6ded7998fb51ce04d0e49becb45a866b49b
Bar0com <florian.krause001@gmail.com>
2024-06-11 20:07:21 +0200

    First Commit for the Eye Tracking Data Analysis: - DeepLabCut setup, training, etc. - calculating pupil diameter based on 4 coordinates - creating graph for diameter over time

---
+++ main.py
@@ -0,0 +1,17 @@
+import deeplabcut
+
+
+if __name__ == '__main__':
+    project_name = 'firstVersion'
+    experimenter = 'Flo'
+    list_of_videos = ['C:/LabData/firstVideo.mp4','C:/LabData/secondVideo.mp4','C:/LabData/thirdVideo.mp4']
+    working_directory = 'C:/LabProjects'
+    config_file_path = 'C:/LabProjects/firstVersion-Flo-2024-05-11/config.yaml'
+    #deeplabcut.create_new_project(project=project_name,experimenter=experimenter,videos=list_of_videos,working_directory=working_directory)
+    #deeplabcut.extract_frames(config=config_file_path,mode='automatic',algo='kmeans',userfeedback=False)
+    deeplabcut.label_frames(config_file_path)
+    #deeplabcut.create_training_dataset(config_file_path, augmenter_type='imgaug')
+    #deeplabcut.create_labeled_video('C:/Users/flori/Desktop/First-Try-Flo-2024-05-09/config.yaml','C:/Users/flori/Downloads/m3v1mp4.mp4', fastmode=False, save_frames= True)
+    deeplabcut.train_network(config=config_file_path, shuffle=[1])
+    deeplabcut.evaluate_network(config=config_file_path, Shuffles=[1], plotting=True)
+
