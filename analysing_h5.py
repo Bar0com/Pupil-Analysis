938331f2f6124ad6b493b83e7d598b74cb8a40f6
Florian <florian.krause001@gmail.com>
2024-06-14 00:08:14 +0200

    Converted the code into functions and re-ordered the structure. There is now the main.py which calls all the functions from the other .py files. Also added some more To-Do's.

--- analysing_h5.py
+++ analysing_h5.py
@@ -3,75 +3,89 @@ import pandas as pd
 import numpy
 import matplotlib.pyplot as plt
 
-filename = r"D:\OneDrive\Documents\University of Amsterdam\Internship\DeepLabCutData\firstVideoDLC_resnet50_firstVersionMay23shuffle1_10000.h5"
 
+def read_in_h5(filename):
+    h5_df = pd.read_hdf(filename)
+    h5_df = h5_df.droplevel(0, axis=1)
+    return h5_df
 
-h5_df = pd.read_hdf(filename)
-h5_df = h5_df.droplevel(0, axis=1)
+def calculate_pupil_diameter(pupil_coordinates):
+    # Accessing x and y data for EyeNorth
+    eye_north_x = pupil_coordinates['EyeNorth']['x']
+    eye_north_y = pupil_coordinates['EyeNorth']['y']
+    eye_north_likelihood = pupil_coordinates['EyeNorth']['likelihood']
 
-# Accessing x and y data for EyeNorth
-eye_north_x = h5_df['EyeNorth']['x']
-eye_north_y = h5_df['EyeNorth']['y']
-eye_north_likelihood = h5_df['EyeNorth']['likelihood']
+    # Accessing x and y data for EyeSouth
+    eye_south_x = pupil_coordinates['EyeSouth']['x']
+    eye_south_y = pupil_coordinates['EyeSouth']['y']
+    eye_south_likelihood = pupil_coordinates['EyeSouth']['likelihood']
 
-# Accessing x and y data for EyeSouth
-eye_south_x = h5_df['EyeSouth']['x']
-eye_south_y = h5_df['EyeSouth']['y']
-eye_south_likelihood = h5_df['EyeSouth']['likelihood']
+    # Accessing x and y data for EyeWest
+    eye_west_x = pupil_coordinates['EyeWest']['x']
+    eye_west_y = pupil_coordinates['EyeWest']['y']
+    eye_west_likelihood = pupil_coordinates['EyeWest']['likelihood']
 
-# Accessing x and y data for EyeWest
-eye_west_x = h5_df['EyeWest']['x']
-eye_west_y = h5_df['EyeWest']['y']
-eye_west_likelihood = h5_df['EyeWest']['likelihood']
+    # Accessing x and y data for EyeEast
+    eye_east_x = pupil_coordinates['EyeEast']['x']
+    eye_east_y = pupil_coordinates['EyeEast']['y']
+    eye_east_likelihood = pupil_coordinates['EyeEast']['likelihood']
 
-# Accessing x and y data for EyeEast
-eye_east_x = h5_df['EyeEast']['x']
-eye_east_y = h5_df['EyeEast']['y']
-eye_east_likelihood = h5_df['EyeEast']['likelihood']
+    # Calculating pupil diameter and writing it in a list
+    diameter_list = []
+    for i in range(len(pupil_coordinates.index)):
+        d = (numpy.sqrt((eye_north_x[i] - eye_south_x[i]) ** 2 + (eye_north_y[i] - eye_south_y[i]) ** 2) +
+             numpy.sqrt((eye_west_x[i] - eye_east_x[i]) ** 2 + (eye_west_y[i] - eye_east_y[i]) ** 2)) / 2
+        diameter_list.append(d)
 
+    # Adding the diameter list to the original Dataframe
+    pupil_coordinates['EyeTotal', 'Diameter'] = diameter_list
+    return pupil_coordinates
 
-# Adding timestamps to frames based on frame rate of video
-frame_rate = 30
-total_frames = len(h5_df)
-timestamp_sec = []
-timestamp_min = []
-for frames in range(total_frames):
-    timestamp_sec.append(frames/frame_rate)
-    timestamp_min.append(frames/frame_rate/60)
-h5_df['Timestamp', 'Seconds'] = timestamp_sec
-h5_df['Timestamp', 'Minutes'] = timestamp_min
-
-
-# Calculating pupil diameter and writing it in a list
-diameter_df = []
-for i in range(len(h5_df.index)):
-    d = (numpy.sqrt((eye_north_x[i] - eye_south_x[i])**2 + (eye_north_y[i] - eye_south_y[i])**2) +
-         numpy.sqrt((eye_west_x[i] - eye_east_x[i])**2 + (eye_west_y[i] - eye_east_y[i])**2)) / 2
-    diameter_df.append(d)
-
-# Adding the diameter list to the original Dataframe
-h5_df['EyeTotal', 'Diameter'] = diameter_df
-
-# Some fun calculations with the diameter
-#std = h5_df['EyeTotal', 'Diameter'].std()
-#pre_mean = h5_df['EyeTotal', 'Diameter'].mean()
-#pre_min = h5_df['EyeTotal', 'Diameter'].min()
-#pre_max = h5_df['EyeTotal', 'Diameter'].max()
+def add_timestamps(data_df, frame_rate, total_frames):
+    # Adding timestamps to frames based on frame rate of video
+    timestamp_sec = []
+    timestamp_min = []
+    for frames in range(total_frames):
+        timestamp_sec.append(frames/frame_rate)
+        timestamp_min.append(frames/frame_rate/60)
+    data_df['Timestamp', 'Seconds'] = timestamp_sec
+    data_df['Timestamp', 'Minutes'] = timestamp_min
+    return data_df
 
 # Outlier detection based on "arbitrary" borders and creation of a cleaned DF by excluding outliers
 # TO DO:
 #   - instead of dropping the data, first determine start and end of actual trials, then interpolate dropped values
 #   - add "blink" marker, which identifies when the eyelids are closed to also interpolate these data points
+def identify_diameter_outliers(data_df, max_diameter, min_diameter):
+    outlier_condition = (data_df['EyeTotal', 'Diameter'] < min_diameter) | (data_df['EyeTotal', 'Diameter'] > max_diameter)
+    outlier_values = data_df.loc[outlier_condition, ('EyeTotal', 'Diameter')]
+    return outlier_values
 
-outlier_condition = (h5_df['EyeTotal', 'Diameter'] < 22) | (h5_df['EyeTotal', 'Diameter'] > 48)
-outlier_values = h5_df.loc[outlier_condition, ('EyeTotal', 'Diameter')]
-clean_df = h5_df.drop(index=outlier_values.index)
-print(clean_df.head(n=10).to_string(index=False))
-# print(clean_df[clean_df['EyeNorth']['likelihood'] < 0.96])
+# TO-DO: Rethink in which order/steps to identify outliers and then actually drop them because interpolation has to
+# happen in-between these steps
+def drop_outliers(data_df, outlier_values):
+    clean_df = data_df.drop(index=outlier_values.index)
+    return clean_df
 
-# Plotting the Diameter over time
-graph = clean_df.plot(x=('Timestamp', 'Minutes'), y=('EyeTotal', 'Diameter'), kind='line')
-plt.show()
+
+# Interpolation
+# Ideas: Take average of previous 5 points and average of next 5 points and then linear interpolate between those
+# Contra: Each tick is 33ms so 5 points already corresponds to 165ms
+def drop_start_data(data_df, likelihood_threshold):
+    for i in range(len(data_df['LED']['likelihood'])):
+        if data_df['LED']['likelihood'][i] > likelihood_threshold:
+            data_df.loc[0:i-1].drop()
+            return data_df
+    print("No start data was dropped")
+
+def interpolate_outliers():
+    1==1
+
+
+def plot_pupil_diameter(data_df):
+    # Plotting the Diameter over time
+    graph = data_df.plot(x=('Timestamp', 'Minutes'), y=('EyeTotal', 'Diameter'), kind='line')
+    plt.show()
 
 
 
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import deeplabcut
+import analysing_h5
 
+filename = r"D:\OneDrive\Documents\University of Amsterdam\Internship\DeepLabCutData\firstVideoDLC_resnet50_firstVersionMay23shuffle1_10000.h5"
+frame_rate = 30
+total_frames = 16810
+max_diameter = 48
+min_diameter = 22
 
 if __name__ == '__main__':
-    project_name = 'firstVersion'
-    experimenter = 'Flo'
-    list_of_videos = ['C:/LabData/firstVideo.mp4','C:/LabData/secondVideo.mp4','C:/LabData/thirdVideo.mp4']
-    working_directory = 'C:/LabProjects'
-    config_file_path = 'C:/LabProjects/firstVersion-Flo-2024-05-11/config.yaml'
-    #deeplabcut.create_new_project(project=project_name,experimenter=experimenter,videos=list_of_videos,working_directory=working_directory)
-    #deeplabcut.extract_frames(config=config_file_path,mode='automatic',algo='kmeans',userfeedback=False)
-    deeplabcut.label_frames(config_file_path)
-    #deeplabcut.create_training_dataset(config_file_path, augmenter_type='imgaug')
-    #deeplabcut.create_labeled_video('C:/Users/flori/Desktop/First-Try-Flo-2024-05-09/config.yaml','C:/Users/flori/Downloads/m3v1mp4.mp4', fastmode=False, save_frames= True)
-    deeplabcut.train_network(config=config_file_path, shuffle=[1])
-    deeplabcut.evaluate_network(config=config_file_path, Shuffles=[1], plotting=True)
-
+    source_data = analysing_h5.read_in_h5(filename)
+    pupil_diameter_df = analysing_h5.calculate_pupil_diameter(source_data)
+    pupil_diameter_df = analysing_h5.add_timestamps(pupil_diameter_df, frame_rate, total_frames)
+    outliers = analysing_h5.identify_diameter_outliers(pupil_diameter_df, max_diameter, min_diameter)
+    clean_pupil_diameter_df = analysing_h5.drop_outliers(pupil_diameter_df, outliers)
+    # Drop start data, still need to re-train model: analysing_h5.drop_start_data()
+    analysing_h5.plot_pupil_diameter(clean_pupil_diameter_df)
+    print(clean_pupil_diameter_df.head(n=10).to_string(index=False))
