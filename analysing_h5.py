46099adc60dfb975377abbc71d04130df1f57d73
Bar0com <florian.krause001@gmail.com>
2024-09-08 14:31:04 +0200

    added a few new steps:   - split data into trial-by-trial data   - added some simple plots   - smoothing the signal using the savitzky-golay filter

--- analysing_h5.py
+++ analysing_h5.py
@@ -1,13 +1,16 @@
-import h5py
 import pandas as pd
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
+import scipy
+
+from math import factorial
 
 
 def read_in_h5(filename):
     h5_df = pd.read_hdf(filename)
     h5_df = h5_df.droplevel(0, axis=1)
     return h5_df
+
 
 def calculate_pupil_diameter(pupil_coordinates):
     # Accessing x and y data for EyeNorth
@@ -33,13 +36,14 @@ def calculate_pupil_diameter(pupil_coordinates):
     # Calculating pupil diameter and writing it in a list
     diameter_list = []
     for i in range(len(pupil_coordinates.index)):
-        d = (numpy.sqrt((eye_north_x[i] - eye_south_x[i]) ** 2 + (eye_north_y[i] - eye_south_y[i]) ** 2) +
-             numpy.sqrt((eye_west_x[i] - eye_east_x[i]) ** 2 + (eye_west_y[i] - eye_east_y[i]) ** 2)) / 2
+        d = (np.sqrt((eye_north_x[i] - eye_south_x[i]) ** 2 + (eye_north_y[i] - eye_south_y[i]) ** 2) +
+             np.sqrt((eye_west_x[i] - eye_east_x[i]) ** 2 + (eye_west_y[i] - eye_east_y[i]) ** 2)) / 2
         diameter_list.append(d)
 
     # Adding the diameter list to the original Dataframe
     pupil_coordinates['EyeTotal', 'Diameter'] = diameter_list
     return pupil_coordinates
+
 
 def add_timestamps(data_df, frame_rate, total_frames):
     # Adding timestamps to frames based on frame rate of video
@@ -52,6 +56,7 @@ def add_timestamps(data_df, frame_rate, total_frames):
     data_df['Timestamp', 'Minutes'] = timestamp_min
     return data_df
 
+
 # Outlier detection based on "arbitrary" borders and creation of a cleaned DF by excluding outliers
 # TO DO:
 #   - instead of dropping the data, first determine start and end of actual trials, then interpolate dropped values
@@ -61,11 +66,18 @@ def identify_diameter_outliers(data_df, max_diameter, min_diameter):
     outlier_values = data_df.loc[outlier_condition, ('EyeTotal', 'Diameter')]
     return outlier_values
 
+
 # TO-DO: Rethink in which order/steps to identify outliers and then actually drop them because interpolation has to
 # happen in-between these steps
 def drop_outliers(data_df, outlier_values):
     clean_df = data_df.drop(index=outlier_values.index)
     return clean_df
+
+
+def drop_unimportant_columns(data_df, unimportant_columns):
+    for column in unimportant_columns:
+        data_df = data_df.drop(column, axis=1, level=0)
+    return data_df
 
 
 # Interpolation
@@ -78,15 +90,120 @@ def drop_start_data(data_df, likelihood_threshold):
             return data_df
     print("No start data was dropped")
 
+
 def interpolate_outliers():
-    1==1
+    1 == 1
 
 
-def plot_pupil_diameter(data_df):
+def plot_pupil_diameter(data_df, ax):
     # Plotting the Diameter over time
-    graph = data_df.plot(x=('Timestamp', 'Minutes'), y=('EyeTotal', 'Diameter'), kind='line')
+    data_df.plot(x=('Timestamp', 'Minutes'),
+                 y=('EyeTotal', 'Diameter'),
+                 kind='line',
+                 ax=ax,
+                 label='unsmooth')
+
+def smoothing_savitzky_golay(x, window_size, order, deriv=0):
+    final_signal = scipy.signal.savgol_filter(x,window_size,order,deriv)
+    return final_signal
+
+
+def plot_smooth_graph(data_df, signal_arr, ax):
+    diameter_plot_smooth = pd.DataFrame({'Timestamp': data_df['Timestamp']['Minutes'],
+                                         'Diameter': signal_arr})
+    smooth_graph = diameter_plot_smooth.plot(x='Timestamp',
+                          y='Diameter',
+                          kind='line',
+                          ax=ax,
+                          label='smooth',
+                          color='red')
+
+
+def merge_smoothed_data(data_df, smoothed_signal):
+    data_df.loc[:, ('EyeTotal', 'Diameter')] = smoothed_signal
+    return data_df
+
+
+def break_into_trials(df):
+    # Initialize an empty list to store trial data
+    trial_data = []
+    trial_count = 1
+
+    # Iterate through the dataframe row by row (starting from the second row to compare with the previous row)
+    for i in range(1, len(df)):
+        # Get current and previous likelihood values using iloc (position-based)
+        current_likelihood = df.iloc[i][('LED', 'likelihood')]
+        previous_likelihood = df.iloc[i - 1][('LED', 'likelihood')]
+
+        # Condition 1: If current likelihood > 0.9 and previous likelihood <= 0.9 (Start condition)
+        if current_likelihood > 0.9 and previous_likelihood <= 0.9:
+            # Store the start of a trial
+            start_time = df.iloc[i][('Timestamp', 'Minutes')]
+
+        # Condition 2: If current likelihood <= 0.9 and previous likelihood > 0.9 (End condition)
+        elif current_likelihood <= 0.9 and previous_likelihood > 0.9:
+            # Store the end of the trial and append the trial data to the list
+            end_time = df.iloc[i - 1][('Timestamp', 'Minutes')]
+            trial_data.append({'Trial': trial_count, 'Start': start_time, 'End': end_time})
+            trial_count += 1  # Move to the next trial
+
+    # Convert the trial_data list to a DataFrame
+    trial_df = pd.DataFrame(trial_data)
+
+    return trial_df
+
+# Start Time in Trial_DF is saved in minutes, so 3s are subtracted from the minutes to get the baseline
+# as there won't be always an exactly perfectly matching timestamp, the one closes to the calculated timestamp
+# will be chosen
+def estimate_baseline(data_df, trial_df):
+    i = 0
+    for minutes in trial_df['Start']:
+        baseline_timestamp = minutes - (3 / 60)
+        closest_timestamp = (data_df['Timestamp']['Minutes'] - baseline_timestamp).abs().idxmin()
+        baseline_diameter = data_df.loc[closest_timestamp, ('EyeTotal', 'Diameter')]
+        trial_df.loc[i, 'Baseline'] = baseline_diameter
+        i += 1
+    return trial_df
+
+
+def add_performance_data(pupil_df, behav_data):
+    i = 0
+    for trial in pupil_df['Trial']:
+        pupil_df.loc[i, 'Correct'] = behav_data.iloc[i,1]
+        i += 1
+    return pupil_df
+
+
+def plot_performance_graph(trial_df, data_df, ax):
+    # Example data for the line plot
+    x = data_df['Timestamp']['Minutes']
+    y = data_df['EyeTotal']['Diameter']
+
+    # Plot the line
+    plt.plot(x, y, label='Pupil Diameter', color=(0.5,0.5,0.5))
+
+    x_base = trial_df['End']
+    y_base = trial_df['Baseline']
+    moving_average = trial_df['Baseline'].rolling(window=4).mean()
+    plt.plot(x_base, moving_average, label='Baseline Diameter', color='#de2d26', linewidth=3)
+
+
+    # Scatter plot data (values on x-axis and fixed y=0)
+    #x_scatter = trial_df['End']
+    #y_scatter = np.full((len(x_scatter)), 56)
+    #colors = ['green' if trial_df['Correct'][i] == 1 else 'red' for i in range(len(x_scatter))]
+
+    # Add scatter plot on the same figure
+    #plt.scatter(x_scatter, y_scatter, s=50, edgecolor=colors, facecolors='none', label='Success')
+
+    # Add labels, legend, and title
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.xlabel('Time (in min)', fontsize=20)
+    plt.ylabel('Pupil Diameter (a.u.)', fontsize=20)
+    plt.legend(fontsize=20)
+
+    # Show the plot
     plt.show()
-
-
 
 
--- main.py
+++ main.py
@@ -1,17 +1,55 @@
-import analysing_h5
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas
+import pandas as pd
 
-filename = r"D:\OneDrive\Documents\University of Amsterdam\Internship\DeepLabCutData\firstVideoDLC_resnet50_firstVersionMay23shuffle1_10000.h5"
+import analysing_h5 as h5
+
+# FOR FINAL EXPERIMENTS:
+    # 1. currently the LED is on for up to 5.7s but for the final experiment we can hardcode that after trial onset the
+    # maximum time of a trial will be 2s
+    # 2. for the final experiments also the last X amount of trials will be deleted (5, 10 or 15 or smth), this will
+    # later also be hard coded
+    # 3. Before Stimulus onset we look at the baseline that is Stimulus onset - 3s, so we will hardcode -3s before
+    # stimulus onset as the baseline
+
+
+# filename = r"D:\OneDrive\Documents\University of Amsterdam\Internship\DeepLabCutData\firstVideoDLC_resnet50_firstVersionMay23shuffle1_10000.h5"
+filename = r'C:\Users\flori\OneDrive\Documents\University of Amsterdam\Internship\DeepLabCutData\Video_02_06_24_croppedDLC_resnet50_secondVersionJun13shuffle1_26000.h5'
+behav_results = pandas.read_csv(r'C:\Users\flori\PycharmProjects\DeepLabCut_v1\behav_performance.csv', header=None)
 frame_rate = 30
-total_frames = 16810
-max_diameter = 48
+total_frames = 117456
+max_diameter = 55
 min_diameter = 22
+unimportant_columns = ['EyeNorth', 'EyeSouth', 'EyeWest', 'EyeEast', 'EyeLidNorth', 'EyeLidSouth']
+
 
 if __name__ == '__main__':
-    source_data = analysing_h5.read_in_h5(filename)
-    pupil_diameter_df = analysing_h5.calculate_pupil_diameter(source_data)
-    pupil_diameter_df = analysing_h5.add_timestamps(pupil_diameter_df, frame_rate, total_frames)
-    outliers = analysing_h5.identify_diameter_outliers(pupil_diameter_df, max_diameter, min_diameter)
-    clean_pupil_diameter_df = analysing_h5.drop_outliers(pupil_diameter_df, outliers)
-    # Drop start data, still need to re-train model: analysing_h5.drop_start_data()
-    analysing_h5.plot_pupil_diameter(clean_pupil_diameter_df)
-    print(clean_pupil_diameter_df.head(n=10).to_string(index=False))
+    # reading in h5 data
+    source_data = h5.read_in_h5(filename)
+
+    # calculating pupil diameter and adding it to df
+    pupil_diameter_df = h5.calculate_pupil_diameter(source_data)
+
+    # adding timestamps column to diameter df
+    pupil_diameter_df = h5.add_timestamps(pupil_diameter_df, frame_rate, total_frames)
+
+    # identify and remove outliers and drop columns that are no longer needed
+    outliers = h5.identify_diameter_outliers(pupil_diameter_df, max_diameter, min_diameter)
+    clean_pupil_diameter_df = h5.drop_outliers(pupil_diameter_df, outliers)
+    clean_pupil_diameter_df = h5.drop_unimportant_columns(clean_pupil_diameter_df, unimportant_columns)
+
+    # smoothing the pupil diameter data, plotting it and adding it to the pupil-diameter df
+    diameter_signal_smooth = h5.smoothing_savitzky_golay(np.array(clean_pupil_diameter_df['EyeTotal']['Diameter']), 50, 2)
+    fig, ax = plt.subplots()
+    #diameter_plot_normal = h5.plot_pupil_diameter(clean_pupil_diameter_df, ax)
+    smooth_df = h5.merge_smoothed_data(clean_pupil_diameter_df, diameter_signal_smooth)
+    #diameter_plot_smooth = h5.plot_smooth_graph(clean_pupil_diameter_df, diameter_signal_smooth, ax)
+
+    # breaking down the entire dataset into trials based on likelihood of LED
+    trial_data = h5.break_into_trials(smooth_df)
+    trial_data = h5.estimate_baseline(smooth_df, trial_data)
+    trial_data = h5.add_performance_data(trial_data, behav_results)
+    performance_plot = h5.plot_performance_graph(trial_data, smooth_df, ax)
+    plt.show()
+    #print(trial_data)
